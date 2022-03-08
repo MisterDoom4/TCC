1bd10e10f61e11f338a6efe9c9917249e5c63b0d
Vinícius Barbosa Mourão <34627836+MisterDoom4@users.noreply.github.com>
2022-03-08 17:12:55 -0400

    Animação inicial resolvida

--- algoritmos/js/servidor_central.js
+++ algoritmos/js/servidor_central.js
@@ -1,48 +1,49 @@
 // primeiro desenho //
 var first = d3.select("#firstsvg")
-                .append("svg")
-                .attr("width", 500)
-                .attr("height", 300);
-var i, radio = 15, distx, n1 = 3, partic = 0, x0,y0,x1,y1;
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 300);
+var i, radio = 15, distx, n1 = 3, partic = 0, y0, x0, x1, y1;
 var part;
 var nodesFirst = [];
 var color = "#E0FF33", line = "RoyalBlue";
 var serverColor = "#2892D7";
 var lineC = "#454545", colorC = "#ffffff";
 
-function desenhoinit(){
+function desenhoinit() {
     y0 = 20; // posição y, fixa
-    x0 = 250;        
+    x0 = 250;
     // função para criar outros processos //
-    for(i=0;i<n1;i++){
-        if(n1<4){
+    for (i = 0; i < n1; i++) {
+        if (n1 < 4) {
             distx = 190;
-            x1=distx+i*60;
-            y1=150;
+            x1 = distx + i * 60;
+            y1 = 150;
         }
-        else{
+        else {
             distx = distx - 30;
-            if(i==0){
-                distx = (n1-1)*30;
-                x1=x0 - distx;
-                y1=150;
+            if (i == 0) {
+                distx = (n1 - 1) * 30;
+                x1 = x0 - distx;
+                y1 = 150;
             }
-            else{
-                x1=x1 + 60;
-                y1=150;
+            else {
+                x1 = x1 + 60;
+                y1 = 150;
             }
-            
+
         }
-        var newNode = {x: x1, y: y1, id: i, distx, part: partic};
+        var newNode = { x: x1, y: y1, id: i, distx, part: partic };
         nodesFirst.push(newNode);
         first.append("line")
-             .attr("x1",x0)
-             .attr("y1",y0)
-             .attr("x2",x1)
-             .attr("y2",y1)
-             .attr("stroke", lineC)
-             .attr("stroke-width", 5)
-             .attr("fill", colorC);
+            .attr("x1", x0)
+            .attr("y1", y0)
+            .attr("x2", x1)
+            .attr("y2", y1)
+            .attr("stroke", lineC)
+            .attr("stroke-width", 5)
+            .attr("fill", colorC);
+
         first.append("circle")
             .attr("cx", x1)
             .attr("cy", y1)
@@ -51,73 +52,109 @@ function desenhoinit(){
             .attr("stroke-width", 1)
             .attr("fill", color)
             .append("text")
-            .text("P"+i);
+            .text("P" + i);
+
     }
+    // desenhar o token //
+    first.append("circle")
+        .attr("class", "token")
+        .attr("cx", x1)
+        .attr("cy", y1)
+        .attr("r", 5)
+        .attr("stroke", lineC)
+        .attr("fill", colorC);
     // primeiro circulo, esse é o servidor central //
     first.append("circle")
-         .attr("cx", x0)
-         .attr("cy", y0)
-         .attr("r", radio)
-         .attr("stroke", line)
-         .attr("stroke-width", 1)
-        .attr("fill", serverColor);
+        .attr("cx", x0)
+        .attr("cy", y0)
+        .attr("r", radio)
+        .attr("stroke", line)
+        .attr("stroke-width", 1)
+        .attr("fill", serverColor)
     first.append("rect")
-         .attr("x", x0 - 38)
-         .attr("y", y0 + 7)
-         .attr("width", 88)
-         .attr("height", 15)
-         .attr("stroke", lineC)
-         .attr("stroke-width", 1)
-         .attr("fill", colorC);
-    first.append("text")
-         .attr("x", x0 - 38)
-         .attr("y", y0 + 17)
-         .text("Servidor Central")
-         .attr("font-family", "sans-serif")
-         .attr("font-size", "12px")
-         .attr("fill", lineC);
-    first.append("rect")
-         .attr("x", x0 - 5)
-         .attr("y", y0)
-         .attr("width", 10)
-         .attr("height", 10)
-         .attr("stroke", lineC)
-       
+        .attr("x", x0 - 38)
+        .attr("y", y0 + 7)
+        .attr("width", 88)
+        .attr("height", 15)
+        .attr("stroke", lineC)
+        .attr("stroke-width", 1)
         .attr("fill", colorC);
+    first.append("text")
+        .attr("x", x0 - 38)
+        .attr("y", y0 + 17)
+        .text("Servidor Central")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "12px")
+        .attr("fill", lineC);
+}
+// function algoritmoSC(){
+//     d3.select("#firstsvg").selectAll(".token")
+//         .transition()
+//         .duration(5000)
+//         .attr("cx", x0)
+//         .attr("cy", y0);
+// }
+
+function playAlg() {
+    d3.select("#firstsvg").selectAll(".token")
+        .transition()
+        .duration(5000)
+        .attr("cx", x0)
+        .attr("cy", y0);
+       
+
+
+    // d3.select("#buttonsR").append("button")
+    //                         .attr("onclick","algoritmoSC()")
+    //                         .attr("class","btn btn-rounded btn-light btn-sm")
+    //                         .attr("id","algoritmo");
+    // .text("Avançar");
+
+    //  d3.select("#buttonsR").append("button")
+    //                         .attr("onclick","restart()")
+    //                         .attr("class","btn btn-rounded btn-light btn-sm")
+    //                         .attr("id","algoritmo")
+    //                         .text("Restart");                     
+    // d3.select("#buttonsR").append("button")
+    //                         .attr("onclick","reset()")
+    //                         .attr("class","btn btn-rounded btn-light btn-sm")
+    //                         .attr("id","algoritmo")
+    //                         .text("Reset");
 }
 // mostrar o nome dos processos //
-function showPart(){
-    for(i=0;i<n1;i++){                
+function showPart() {
+    for (i = 0; i < n1; i++) {
         first.append("rect")
-                .attr("x", nodesFirst[i].x - 8)
-                .attr("y", nodesFirst[i].y + 7)
-                .attr("width", 16)
-                .attr("height", 15)
-                .attr("stroke", lineC)
-                .attr("stroke-width", 1)
-                .attr("fill", colorC);
+            .attr("x", nodesFirst[i].x - 8)
+            .attr("y", nodesFirst[i].y + 7)
+            .attr("width", 16)
+            .attr("height", 15)
+            .attr("stroke", lineC)
+            .attr("stroke-width", 1)
+            .attr("fill", colorC);
         first.append("text")
-                .attr("x", nodesFirst[i].x -8)
-                .attr("y", nodesFirst[i].y + 17)
-                .text("P"+i)
-                .attr("font-family", "sans-serif")
-                .attr("font-size", "12px")
-                .attr("fill", lineC);
+            .attr("x", nodesFirst[i].x - 8)
+            .attr("y", nodesFirst[i].y + 17)
+            .text("P" + i)
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "12px")
+            .attr("fill", lineC);
+
     }
 }
 // voltar para o estado inicial, sem os nomes dos processos //
-function unshowPart(){
+function unshowPart() {
+
     d3.select("#firstsvg").selectAll("rect").remove();
     d3.select("#firstsvg").selectAll("text").remove();
-    nodesFirst.splice(0,10);
+    nodesFirst.splice(0, 10);
     desenhoinit();
 }
 // função para utilizar o slide, para mudar o desenho //
 var slider1 = document.getElementById("sliderNodePart");
-    slider1.oninput = function (){
-    n1=this.value;
-    nodesFirst.splice(0,10);
-    console.log(nodesFirst);
+slider1.oninput = function () {
+    n1 = this.value;
+    nodesFirst.splice(0, 10);
     d3.select("#firstsvg").selectAll("circle").remove();
     d3.select("#firstsvg").selectAll("text").remove();
     d3.select("#firstsvg").selectAll("rect").remove();
@@ -127,7 +164,7 @@ var slider1 = document.getElementById("sliderNodePart");
 // inicialização da tela //
 window.onload = desenhoinit();
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
--- algoritmos/js/teste.js
+++ algoritmos/js/teste.js
@@ -1,45 +1,38 @@
-var desenho = d3.select("#testesvg")
+var desenho = d3.select("#rasvg")
                 .append("svg")
                 .attr("width", 540)
                 .attr("height", 300)
                 .style("background","red");
-var n1 = 3,x0 = 230,y0 = 0, rx0 = 15, ry0 = 15, tx = 80, ty = 80;
+var n1 = 3,x0 = 230,y0 = 0, tx = 100, ty = 60;
                 
 function iniciar(){
-    desenho.append("rect")		// pre-defined shape
-            .attr("style", "fill:darkblue")	// fill color of shape
+  desenho.append("rect")		// pre-defined shape
+            .attr("style", "fill:black")	// fill color of shape
             .attr("x", x0)					// displacement from origin
             .attr("y", y0)					// displacement from origin
-            .attr("rx", rx0)					// how much to round corners - to be transitioned below
-            .attr("ry", ry0)					// how much to round corners - to be transitioned below
             .attr("width", tx)				// size of shape
             .attr("height", ty);
+  desenho.append("text")
+            .attr("x", 230)
+            .attr("y", 22)
+            .text("Processo 1")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "18px")
+            .attr("fill","#FFFAFA");
+  desenho.append("rect")		// pre-defined shape
+            .attr("style", "fill:pink")	// fill color of shape
+            .attr("x", x0)					// displacement from origin
+            .attr("y", 30)					// displacement from origin
+            .attr("width", tx)				// size of shape
+            .attr("height", 30);
+  desenho.append("text")
+            .attr("x", 230)
+            .attr("y", 52)
+            .text("RELEASED")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "18px")
+            .attr("fill","#000000");
 }		
-
-var slider1 = document.getElementById("sliderNodePart2");
-slider1.oninput = function (){
-n1=this.value;
-console.log("ola");
-mudar();
-// d3.select("#testesvg").selectAll("rect").remove();
-}
-  function mudar(){
-    if(n1 % 2 == 0){
-    d3.select("#testesvg").selectAll("rect")
-    .transition()
-    .duration(3000)
-    .attr("rx",0)
-    .attr("ry",0);
-    }
-    else{
-    d3.select("#testesvg").selectAll("rect")
-    .transition()
-    .duration(3000)
-    .attr("rx",rx0)
-    .attr("ry",ry0);
-    }
-}
-
 window.onload = iniciar();
 
                 
