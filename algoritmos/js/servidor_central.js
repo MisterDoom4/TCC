f14cb871e4a40f1ace4375930fc7ccb6be816c8a
Julio Sandro <multyroom@gmail.com>
2022-03-17 13:30:04 -0400

    ajustes

--- algoritmos/js/servidor_central.js
+++ algoritmos/js/servidor_central.js
@@ -487,6 +487,9 @@ window.onload = desenhoInit();
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
+    var y = window.scrollY;
+    console.log(y);
+    
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("myBtn").style.display = "block";
 
